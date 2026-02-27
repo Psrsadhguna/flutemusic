9188edb5539eea294bd07daab82d8c97d4e8b96c
Psrsadhguna <saisadhguna3@gmail.com>
2026-02-27 20:18:48 +0530

    main

--- commands/premium.js
+++ commands/premium.js
@@ -10,7 +10,7 @@ module.exports = {
     const isPremium = paymentUtils.isPremium(userId);
     const premiumUser = paymentUtils.getPremiumUser(userId);
 
-    const webURL = process.env.WEBSITE_URL || 'https://rzp.io/rzp/ocbhQOC';
+    const webURL = process.env.WEBSITE_URL || 'https://rzp.io/rzp/KxDGvM0';
 
     const embed = new EmbedBuilder()
       .setTitle('💎 Flute Music Premium')
